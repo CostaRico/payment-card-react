6526b5e5224405af2b76c2cc93436f7749af1d52
Alexander Taranov <a.taranov@smartpeopleteam.com>
2016-07-11 23:01:05 +0600

    added initial values, updated styles

--- src/components/select/select.js
+++ src/components/select/select.js
@@ -1,12 +1,6 @@
 import React from 'react';
 
 export default class Select extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      someText: 'blablabla'
-    }
-  }
   render() {
     return (
         <div className="field-select">
